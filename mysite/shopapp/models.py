cca67c7add3c89e56903d60b5a703e8dc5bcd4ed
Eugene <eugenmos09@gmail.com>
2024-11-06 13:48:00 +0300

    New features

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+"""
+Чтобы изменить версию миграции:
+Terminal
+
+python manage.py migrate shopapp ****
+
+Где **** - необходимая существующая версия из папки migrations (прим. 0003)
+"""
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=False, blank=True)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    discount = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=False)
