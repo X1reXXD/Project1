0d8e9f331ae96207d2e6e70c36679e51ffbc30e8
Eugene <eugenmos09@gmail.com>
2024-11-10 14:32:34 +0300

    New features

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -12,6 +12,13 @@ python manage.py migrate shopapp ****
 
 
 class Product(models.Model):
+    class Meta:
+        ordering = ["name"]
+        # Сортировка продуктов по названию
+
+        # db_table = "tech_products"
+        # verbose_name_plural = "products"
+
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
